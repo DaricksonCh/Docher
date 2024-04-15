bbc29c52065751e3a2b7a7b17500dc5b60de9200
ersanti14 <erazoser14@gmail.com>
2024-04-15 14:43:07 -0500

    vovimos

---
+++ frontend/src/components/Menu.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+export const Menu = () => {
+
+
+    return(
+        <>
+        <h1>dollaa</h1>
+        <button
+        className="btn btn-danger mr-2"> putooooooo</button>
+        </>
+    )
+}
+export default Menu;
+
