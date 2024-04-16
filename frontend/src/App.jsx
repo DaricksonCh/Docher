fb308699b39105dae3880d674daab708ee50f5fb
ersanti14 <erazoser14@gmail.com>
2024-04-15 20:10:09 -0500

    cositas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { Menu } from "./components/Menu";
+import Menu from "./components/Menu";
 
 
 const App = () => {
